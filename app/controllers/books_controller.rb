7fd898ca394e3248e501430fc24eb38ea281890d
Yuka511 <174612970+Yuka511@users.noreply.github.com>
2024-08-29 07:22:42 +0000

    [Middle]リンク機能直し

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,19 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
     @book = Book.new
     @user = current_user
     @books = Book.all
+    render :index
   end
 
   def show
